3e2f0beb57abb01e1262f1ffa962a554430e37fb
Ellis Thompson <ellisthompson.1999@outlook.com>
2020-08-10 00:39:02 +0100

    Update main.cpp
    Refactored to not use "using namespace std"

--- main.cpp
+++ main.cpp
@@ -5,37 +5,35 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 #include <iostream>
 
-using namespace std;
-
 int main(){
 bool loop=true;
 int sum=0;
 int num;
 while(loop==true){
-    cout << "Euler Problem 1" << endl;
-    cout << "Adding up all multiples of 5 and 3  up to 1000" << endl;
+    std::cout << "Euler Problem 1" << std::endl;
+    std::cout << "Adding up all multiples of 5 and 3  up to 1000" << std::endl;
     int fives = 5;
     int threes = 3;
 
         if ((num % fives ==0) && (num % threes==0)){
             sum=sum+num;
-            cout<<num<<endl;
+            std::cout<<num<<std::endl;
         }
         else if (num % fives==0) {
             sum = sum+num;
-            cout << num << endl;
+            std::cout << num << std::endl;
         }
         else if (num % threes == 0){
             sum=sum+num;
-            cout<< num << endl;
+            std::cout<< num << std::endl;
         }
     }
 
-    cout<<"sum is" <<endl;
-    cout<<sum<<endl;
-    cout << "Again?"<<endl;
+    std::cout<<"sum is" <<std::endl;
+    std::cout<<sum<<std::endl;
+    std::cout << "Again?"<<std::endl;
     string answer;
-    cin>>answer;
+    std::cin>>answer;
     if (answer=="no"){
         loop=false;
     }
